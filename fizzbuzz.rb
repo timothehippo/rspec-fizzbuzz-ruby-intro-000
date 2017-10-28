6fed3472fbadd32192a4a783dc04f9b240cee2b3
timothehippo <tyxhul@yahoo.com>
2017-10-28 20:33:34 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,10 +1,10 @@
 def fizzbuzz(int)
 if(int%5==0&&int%3==0)
-  return "fizzbuzz"
+  return "Fizzbuzz"
 elsif(int%3==0)
-  return  "fizz"
+  return  "Fizz"
 elsif(int%5==0)
-  return "buzz"
+  return "Buzz"
 else
   return
 end
